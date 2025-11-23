e6d64298975b72c6f47cebdad61596b5d1360598
raziel <tvindzon@gmail.com>
2025-11-23 18:47:36 +0700

    update commit

--- app/bot/config/models/filed.py
+++ app/bot/config/models/filed.py
@@ -7,20 +7,22 @@ class FieldGeneration(BaseModel):
 
     # Основные параметры - обязательны к заполнению
     SERVICE_NAME: str = ""  # имя для роутера
-    SERVICE_ID: str = ""  # параметр нужный для определеия путей, кнопок
+    SERVICE_ID: str = ""  # параметр нужный для определеия путей
 
     BOT_DIR: Path = Path(__file__).resolve().parent.parent.parent
 
     # Данные кнопок - обязательны к заполнению
     TEXT_PREFIX: str = ""
+    TEXT_DATA: str = ""
     CALLBACK_PREFIX: str = ""
+    CALLBACK_DATA: str = ""
 
     # Текст каллбэк кнопки
     @property
     def CALLBACK_BUTTON_TEXT(self):
-        return f"{self.TEXT_PREFIX} {self.SERVICE_ID}"
+        return f"{self.TEXT_PREFIX} {self.TEXT_DATA}"
 
     # Данные каллбэк кнопки
     @property
     def CALLBACK_BUTTON_DATA(self):
-        return f"{self.CALLBACK_PREFIX} {self.SERVICE_ID}"
+        return f"{self.CALLBACK_PREFIX} {self.CALLBACK_DATA}"
